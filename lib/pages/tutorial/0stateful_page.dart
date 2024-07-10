ca874b4beab152c56f721dd2c96edf6ec128761a
sehanaf4 <sehanaf4@gmail.com>
2024-07-10 02:31:48 +0000

    update dengan menambahkan komentar & penjelasan

--- lib/pages/tutorial/0stateful_page.dart
+++ lib/pages/tutorial/0stateful_page.dart
@@ -1,54 +1,69 @@
 import 'package:flutter/material.dart';
 
+// StatefulPage adalah StatefulWidget, yang berarti widget ini bisa berubah
+// karena memiliki state yang dapat diperbarui.
 class StatefulPage extends StatefulWidget {
+  // Konstruktor untuk StatefulPage.
   StatefulPage({super.key});
 
   @override
   State<StatefulPage> createState() => _StatefulPageState();
 }
 
+// _StatefulPageState adalah state dari StatefulPage. Di sinilah logika 
+// untuk perubahan state ditulis.
 class _StatefulPageState extends State<StatefulPage> {
+  // Variabel counter untuk menyimpan nilai yang akan ditampilkan dan diubah.
   int counter = 1;
 
+  // Metode build untuk membangun tampilan UI.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Scaffold menyediakan struktur dasar visual seperti app bar, body, dll.
       home: Scaffold(
+        // AppBar adalah bar di bagian atas aplikasi.
         appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.blueAccent, // Warna latar belakang AppBar.
           title: const Text(
-            "Stateful Widget",
-            style: TextStyle(color: Colors.white),
+            "Stateful Widget", // Teks judul di AppBar.
+            style: TextStyle(color: Colors.white), // Warna teks putih.
           ),
-          centerTitle: true,
+          centerTitle: true, // Judul di tengah.
         ),
+        // Body dari Scaffold berisi kolom (Column) dengan dua anak widget.
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Mengatur jarak antar widget secara merata.
           children: [
+            // Teks yang menampilkan nilai dari counter.
             Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 50),
+              counter.toString(), // Menampilkan nilai counter.
+              style: TextStyle(fontSize: 50), // Ukuran font besar.
             ),
+            // Baris (Row) yang berisi dua tombol untuk mengurangi dan menambah counter.
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Mengatur jarak antar tombol secara merata.
               children: [
+                // Tombol untuk mengurangi nilai counter.
                 ElevatedButton(
                   onPressed: () {
+                    // Jika counter tidak sama dengan 1, kurangi nilai counter.
                     if (counter != 1) {
                       setState(() {
-                        counter--;
+                        counter--; // Mengurangi nilai counter.
                       });
                     }
                   },
-                  child: Icon(Icons.remove),
+                  child: Icon(Icons.remove), // Ikon minus pada tombol.
                 ),
+                // Tombol untuk menambah nilai counter.
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
-                      counter++;
+                      counter++; // Menambah nilai counter.
                     });
                   },
-                  child: Icon(Icons.add),
+                  child: Icon(Icons.add), // Ikon plus pada tombol.
                 ),
               ],
             )
--- lib/pages/tutorial/1mapping_list.dart
+++ lib/pages/tutorial/1mapping_list.dart
@@ -1,14 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
+// MappingList adalah StatelessWidget, yang berarti widget ini tidak memiliki state yang berubah.
 class MappingList extends StatelessWidget {
+  // Konstruktor untuk MappingList.
   MappingList({super.key});
 
+  // Deklarasi list myList yang berisi data berupa Map.
   final List<Map<String, dynamic>> myList = [
     {
       "name": "Sehun",
       "age": 20,
       "gender": "Male",
-      "favColor": ["Red", "Blue", "Black","Red", "Blue", "Black","Red", "Blue", "Black","Red", "Blue", "Black"]
+      "favColor": ["Red", "Blue", "Black", "Red", "Blue", "Black", "Red", "Blue", "Black", "Red", "Blue", "Black"]
     },
     {
       "name": "Alexa",
@@ -26,58 +31,68 @@ class MappingList extends StatelessWidget {
       "name": "Sehun",
       "age": 20,
       "gender": "Male",
-      "favColor": ["Red", "Blue", "Black","Red", "Blue", "Black","Red", "Blue", "Black","Red", "Blue", "Black"]
+      "favColor": ["Red", "Blue", "Black", "Red", "Blue", "Black", "Red", "Blue", "Black", "Red", "Blue", "Black"]
     },
   ];
 
+  // Metode build untuk membangun tampilan UI.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Mapping List"),
-          centerTitle: true,
+          title: Text("Mapping List"), // Judul AppBar.
+          centerTitle: true, // Judul di tengah.
         ),
+        // ListView untuk menampilkan daftar item dari myList.
         body: ListView(
+          // Memetakan setiap elemen di myList ke dalam widget Card.
           children: myList.map((data) {
+            // Mengambil list warna favorit dari data.
             List myfavcolor = data["favColor"];
             return Card(
-              margin: EdgeInsets.all(10),
-              elevation: 10,
+              margin: EdgeInsets.all(10), // Margin di sekitar Card.
+              elevation: 10, // Elevasi untuk memberikan efek bayangan.
+              // Container untuk menampilkan detail dari data.
               child: Container(
-                margin: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10), // Margin di dalam Container.
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start, // Posisi anak-anak widget di mulai dari kiri.
                   children: [
+                    // Baris pertama untuk menampilkan avatar dan detail nama, usia, gender.
                     Row(
                       children: [
-                        CircleAvatar(),
+                        CircleAvatar(), // Avatar lingkaran.
                         SizedBox(
-                          width: 20, 
+                          width: 20, // Spasi horizontal antara avatar dan teks.
                         ),
+                        // Kolom untuk menampilkan detail nama, usia, dan gender.
                         Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start, // Posisi teks di mulai dari kiri.
                           children: [
-                            Text("Name: ${data["name"]}"),
-                            Text("Age: ${data["age"]}"),
-                            Text("Gender: ${data["gender"]}"),
+                            Text("Name: ${data["name"]}"), // Teks untuk nama.
+                            Text("Age: ${data["age"]}"), // Teks untuk usia.
+                            Text("Gender: ${data["gender"]}"), // Teks untuk gender.
                           ],
                         ),
                       ],
                     ),
+                    // SingleChildScrollView untuk menggulung warna favorit secara horizontal.
                     SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
+                      scrollDirection: Axis.horizontal, // Gulir secara horizontal.
+                      // Baris untuk menampilkan warna favorit.
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start, // Posisi anak-anak widget di mulai dari kiri.
                         children: myfavcolor.map((color) {
+                          // Kontainer untuk setiap warna favorit.
                           return Container(
-                            color: Colors.amberAccent,
+                            color: Colors.amberAccent, // Warna latar belakang kontainer.
                             margin: EdgeInsets.symmetric(
-                              vertical: 15,
-                              horizontal: 8,
+                              vertical: 15, // Margin vertikal.
+                              horizontal: 8, // Margin horizontal.
                             ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(color),
+                            padding: EdgeInsets.all(10), // Padding di dalam kontainer.
+                            child: Text(color), // Teks untuk warna.
                           );
                         }).toList(),
                       ),
